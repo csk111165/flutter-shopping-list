43f791d99d2f88976357342a37cf8179db241760
Chandra Shekhar <cshekhar@nvidia.com>
2023-09-19 04:06:02 +0530

    Adding dismissible widget to allow swip direction to delte the list tile

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -27,6 +27,14 @@ class _GroceryListState extends State<GroceryList> {
     });
   }
 
+   void _removeItem(GroceryItem item)
+    {
+      setState(() {
+        _groceryItems.remove(item);
+      });
+    }
+
+
   @override
   Widget build(BuildContext context) {
     // fallback content when the list is empty
@@ -37,14 +45,20 @@ class _GroceryListState extends State<GroceryList> {
     if( _groceryItems.isNotEmpty) {
       content = ListView.builder(
         itemCount: _groceryItems.length,
-        itemBuilder: (context, index) => ListTile(
-          title: Text(_groceryItems[index].name),
-          leading: Container(
-            width: 24,
-            height: 24,
-            color: _groceryItems[index].category.color,
+        itemBuilder: (context, index) => Dismissible(
+          onDismissed: (direction) {
+            _removeItem(_groceryItems[index]);
+          },
+          key: ValueKey(_groceryItems[index].id), // the key should be uniquety identifiable
+          child: ListTile(
+            title: Text(_groceryItems[index].name),
+            leading: Container(
+              width: 24,
+              height: 24,
+              color: _groceryItems[index].category.color,
+            ),
+            trailing: Text(_groceryItems[index].quantity.toString()),
           ),
-          trailing: Text(_groceryItems[index].quantity.toString()),
         ),
       );
     }
