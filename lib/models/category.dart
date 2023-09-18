7bad971b0c17fb82a76ff9af8ad6535dd72b2e4a
Chandra Shekhar <cshekhar@nvidia.com>
2023-09-18 22:03:28 +0530

    Initializing the shopping-list project

---
+++ lib/models/category.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+enum Categories {
+  vegetables,
+  fruit,
+  meat,
+  dairy,
+  carbs,
+  sweets,
+  spices,
+  convenience,
+  hygiene,
+  oher,
+}
+
+
+class Category {
+
+  const Category(this.title, this.color);
+  
+  final String title;
+  final Color color;
+}
