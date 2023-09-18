5048c1426c1cb364a6432f534e7f56b61d473a2a
Chandra Shekhar <cshekhar@nvidia.com>
2023-09-18 22:19:15 +0530

    Displays the GroceryList

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list/widgets/grocery_list.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Groceries',
       theme: ThemeData.dark().copyWith(
         useMaterial3: true,
@@ -21,7 +23,7 @@ class MyApp extends StatelessWidget {
         ),
         scaffoldBackgroundColor: const Color.fromARGB(255, 50, 58, 60),
       ),
-      home: ...,
+      home: const GroceryList(),
     );
   }
 }
--- lib/models/category.dart
+++ lib/models/category.dart
@@ -10,14 +10,14 @@ enum Categories {
   spices,
   convenience,
   hygiene,
-  oher,
+  other,
 }
 
 
 class Category {
 
   const Category(this.title, this.color);
-  
+
   final String title;
   final Color color;
 }
