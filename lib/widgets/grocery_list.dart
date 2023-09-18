3f7536c3be4380426c246f6e6ddae66ebb9c2bdb
Chandra Shekhar <cshekhar@nvidia.com>
2023-09-19 04:00:01 +0530

    Display fallback widget/content in case the list is empty

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:shopping_list/models/grocery_item.dart';
 import 'package:shopping_list/widgets/new_item.dart';
@@ -12,13 +10,16 @@ class GroceryList extends StatefulWidget {
 }
 
 class _GroceryListState extends State<GroceryList> {
-
   final List<GroceryItem> _groceryItems = [];
 
-  void _addItem() async{
-    final newItem = await Navigator.push<GroceryItem>(context, MaterialPageRoute(builder: (context) => const NewItem(),));
+  void _addItem() async {
+    final newItem = await Navigator.push<GroceryItem>(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const NewItem(),
+        ));
     if (newItem == null) {
-      return ;
+      return;
     }
 
     setState(() {
@@ -28,14 +29,13 @@ class _GroceryListState extends State<GroceryList> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Your Groceries'),
-        actions: [
-          IconButton(onPressed: _addItem, icon: const Icon(Icons.add))
-        ],
-      ),
-      body: ListView.builder(
+    // fallback content when the list is empty
+    Widget content = const Center(
+      child: Text('No items added yet.'),
+    );
+
+    if( _groceryItems.isNotEmpty) {
+      content = ListView.builder(
         itemCount: _groceryItems.length,
         itemBuilder: (context, index) => ListTile(
           title: Text(_groceryItems[index].name),
@@ -46,7 +46,14 @@ class _GroceryListState extends State<GroceryList> {
           ),
           trailing: Text(_groceryItems[index].quantity.toString()),
         ),
-        ),
+      );
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Groceries'),
+        actions: [IconButton(onPressed: _addItem, icon: const Icon(Icons.add))],
+      ),
+      body: content,
     );
   }
 }
